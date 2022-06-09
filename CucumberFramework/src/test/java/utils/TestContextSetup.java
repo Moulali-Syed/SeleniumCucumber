441d1bb056fafafccdc67cc8843d01260455b8b8
Moulali Syed <syedmoulali1997@gmail.com>
2022-06-09 11:25:35 +0530

    POM-Factory-SRP

---
+++ CucumberFramework/src/test/java/utils/TestContextSetup.java
@@ -0,0 +1,14 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+
+import pageObjects.PageObjectManager;
+
+public class TestContextSetup {
+	public WebDriver driver;
+	public String landingPageProductName;
+	public PageObjectManager pageObjectManager;
+	public TestContextSetup() {
+		pageObjectManager = new PageObjectManager(driver);
+	}
+}

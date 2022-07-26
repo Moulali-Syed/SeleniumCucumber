3b111901814e599a1e6a74b7a8815ab9ce176533
Moulali Syed <syedmoulali1997@gmail.com>
2022-07-26 18:41:17 +0530

    cucumber

--- CompleteCucumberDemoProject/src/test/java/cucumberOptions/TestNGTestRunner.java
+++ CompleteCucumberDemoProject/src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -5,9 +5,10 @@ import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/java/features", glue = "stepDefinitions", monochrome = true,
-tags = "@PlaceOrder or @OffersPage" , plugin= {"html:target/cucumber.html","json:target/cucumber.json"})
-
+@CucumberOptions(features = "src/test/java/features", glue = "stepDefinitions", monochrome = true, tags = "@PlaceOrder or @OffersPage", plugin = {
+		"html:target/cucumber.html", "json:target/cucumber.json",
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" ,
+		"rerun:target/failed_scenarios.txt"})
 public class TestNGTestRunner extends AbstractTestNGCucumberTests {
 
 	// we are overriding the scenarios method of AbstractTestNGCucumberTests class
